63342635678d7fe6c49afb3ce22d76805d645110
JavAndy58 <digital.designs.russia@gmail.com>
2022-05-16 21:54:55 +0300

    3. HQL [#6874]

--- src/main/java/ru/job4j/gro/HbmRun.java
+++ src/main/java/ru/job4j/gro/HbmRun.java
@@ -6,6 +6,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
+
 import javax.persistence.Query;
 
 public class HbmRun {
@@ -13,13 +14,13 @@ public class HbmRun {
     public static void main(String[] args) {
         SpringApplication.run(HbmRun.class, args);
 
-
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
+
             /*
             Candidate one = new Candidate("Alex", 2.5, 150000);
             Candidate two = new Candidate("Nikolay", 1.5, 100000);
@@ -28,12 +29,12 @@ public class HbmRun {
             session.save(two);
             session.save(three);
              */
+
             /*
             Query query = session.createQuery("from Candidate");
             for (Object candidate : ((org.hibernate.query.Query<?>) query).list()) {
                 System.out.println(candidate);
             }
-
              */
 
             /*
@@ -41,18 +42,27 @@ public class HbmRun {
             query.setParameter("fId", 1);
             System.out.println(((org.hibernate.query.Query<?>) query).uniqueResult());
             */
+
             /*
             Query query = session.createQuery("from Candidate c where c.name = :fName");
             query.setParameter("fName", "Nikita");
             System.out.println(((org.hibernate.query.Query<?>) query).uniqueResult());
             */
 
-            session.createQuery("update Candidate c set c.name = :newName, c.experience = :newExperience, c.salary =:newSalary where c.id = :fId")
+            /*
+            session.createQuery("update Candidate c set c.name = :newName, c.experience = :newExperience, c.salary = :newSalary where c.id = :fId")
                             .setParameter("newName", "Andrey")
-                            .setParameter("newExperience", 1)
-                            .setParameter("newSalary", 50000)
-                            .setParameter("fId", 2)
+                            .setParameter("newExperience", 1.0)
+                            .setParameter("newSalary", 50000.0)
+                            .setParameter("fId", 1)
                             .executeUpdate();
+             */
+
+            /*
+            session.createQuery("delete from Candidate where id = :fId")
+                            .setParameter("fId", 3)
+                            .executeUpdate();
+             */
 
 
             session.getTransaction().commit();
@@ -62,6 +72,5 @@ public class HbmRun {
         } finally {
             StandardServiceRegistryBuilder.destroy(registry);
         }
-
     }
 }
